dcd4e73c3a3c015edf1922214d540c5365a803ff
nizammydin <nizam.mydin@tmrnd.com.my>
2023-12-26 20:31:22 -0500

    updated

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -35,7 +37,6 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-        ## disabling https
         talisman.force_https = False
 
     @classmethod
